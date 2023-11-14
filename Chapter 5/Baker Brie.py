48f9ebd5c7859781616edeba963a76134ec3b75a
GnomesAreReal <ccjosey@goshen.edu>
2023-11-14 12:04:40 -0500

    Baker Brie

--- Chapter 5/Baker Brie.py
+++ Chapter 5/Baker Brie.py
@@ -1,14 +1,23 @@
-Franchises, Days = map(int, input().split())
+for _ in range(10):
+    Franchises, Days = map(int, input().split())
 
-grid = []
-bonuses = 0
+    grid = []
+    dozens = 0
 
-for i in range(Days):
-    grid.append(list(map(int, input().split())))
-print(grid)
+    for i in range(Days):
+        grid.append(list(map(int, input().split())))
 
-for row in grid:
-    # Sum of all franchises for the day
-    if sum(row) % 13 == 0:
-        bonuses += sum(row) // 13
-print(bonuses)
+    for row in grid:
+        # Sum of all franchises for the day
+        if sum(row) % 13 == 0:
+            dozens += sum(row) // 13
+
+    for col_index in range(Franchises):
+        column = []
+        for row_index in range(Days):
+            column.append(grid[row_index][col_index])
+
+        if sum(column) % 13 == 0:
+            dozens += sum(column) // 13
+
+    print(dozens)
